e0a1cc3de98ca8c3557ff43af4626404187f4de9
Uwe Dauernheim <uwe@dauernheim.net>
2013-04-03 18:15:43 +0200

    Add nilable functions

---
+++ src/clore/nilable.clj
@@ -0,0 +1,57 @@
+(ns ^{:author "Uwe Dauernheim <uwe@dauernheim.net>"
+      :doc "Nilable (Nullable) helper functions."}
+  clore.nilable)
+
+(defmacro coalesce
+  "Returns nil if all of its arguments are nil, otherwise it returns the first
+  non-nil argument. Similar to (first (filter #(not (nil? %)) coll)) but takes
+  arguments instead of a collection."
+  {:added "1.0"}
+  ([] nil)
+  ([x] x)
+  ([x & rest]
+     `(let [c# ~x]
+        (if (not (nil? c#))
+          c#
+          (coalesce ~@rest)))))
+
+(defn ?>=
+  "Nilable variant of >=, but also returns false if only first argument is nil
+  or true if all arguments are nil."
+  {:added "1.0"}
+  ([& more]
+     (if (nil? (first more))
+       (apply = more) ; assert all nil
+       (apply >= more)))) ; fallback default behaviour
+
+;?>
+;?<=
+;?<
+;?=
+;?+
+;?-
+;?*
+;?/
+;?%
+;
+;>=?
+;>?
+;<=?
+;<?
+;=?
+;+?
+;-?
+;*?
+;/?
+;%?
+;
+;?>=?
+;?>?
+;?<=?
+;?<?
+;?=?
+;?+?
+;?-?
+;?*?
+;?/?
+;?%?
---
+++ test/clore/nilable_test.clj
@@ -0,0 +1,25 @@
+(ns clore.nilable-test
+  (:use clojure.test clore.nilable))
+
+
+(deftest coalesce-test
+  (is (= false (coalesce nil false 1)))
+  (is (= 1 (coalesce nil 1)))
+  (is (= 1 (coalesce 1 2)))
+  (is (= nil (coalesce (first []))))
+  (is (= nil (coalesce nil nil)))
+  (is (= nil (coalesce))))
+
+(deftest ?>=-test
+  (is (= true (?>= 1)))
+  (is (= true (?>= 2 1)))
+  (is (= true (?>= 3 2 1)))
+  (is (= true (?>= nil)))
+  (is (= false (?>= nil 1)))
+  (is (= false (?>= nil 1 1)))
+  (is (= true (?>= nil nil)))
+  (is (= true (?>= nil nil nil)))
+  (is (= false (?>= 1 2)))
+  (is (= false (?>= 1 2 3)))
+  (is (thrown? clojure.lang.ArityException (?>=)))
+  (is (thrown? NullPointerException (?>= 1 nil))))
