42e1e26821fb7e231e3c0deb58744ca2686b7e2f
Uwe Dauernheim <uwe@dauernheim.net>
2013-06-16 17:25:35 +0200

    Add useful to dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject clojure.contrib-djui "1.2"
+(defproject org.clojure/contrib-djui "1.2"
   :description "Library of helper functions as extension to clojure and clojure.contrib."
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"
             :distribution :repo}
   :url "https://github.com/djui/clojure.contrib-djui"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [egamble/let-else "1.0.5"]])
+                 [egamble/let-else "1.0.5"]
+                 [useful "0.8.8"]])
