064c89cf15d607d7c5054ce5c6b08c8cf3799a46
Uwe Dauernheim <uwe@dauernheim.net>
2013-06-16 17:22:02 +0200

    Update project file and include let-else dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject clore "1.0.0"
-  :description "Clojure utility helper functions."
+(defproject clojure.contrib-djui "1.2"
+  :description "Library of helper functions as extension to clojure and clojure.contrib."
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"
             :distribution :repo}
-  :url "https://github.com/djui/clore"
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :url "https://github.com/djui/clojure.contrib-djui"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [egamble/let-else "1.0.5"]])
--- src/clojure/contrib_djui/core.clj
+++ src/clojure/contrib_djui/core.clj
@@ -1,6 +1,7 @@
 (ns ^{:author "Uwe Dauernheim <uwe@dauernheim.net>"
       :doc "Core helper functions."}
-  clojure.contrib-djui.core)
+  clojure.contrib-djui.core
+  (:use [let-else :only [let?]]))
 
 
 (defn unit
@@ -32,3 +33,43 @@
    :deprecated "1.1"}
   [& args]
   (reduce #(%2 %) args))
+
+(defn fix-point
+  "Repeatedly call f with data-in until f(data-in) equals data-in."
+  [f data-in]
+  (let [data-out (f data-in)]
+    (if (= data-out data-in)
+      data-out ;; Don't return data-in in case meta data was added.
+      (recur f data-out))))
+
+;; Inspired by:
+;; * https://github.com/cgrand/parsley/blob/master/src/net/cgrand/parsley/util.clj
+;; * http://edtsech.github.io/2012/12/and-let.html
+(defmacro if-all-let
+  "Similar to if-let but supports multiple binding terms which have to be true
+  for the binding evaluation to continue."
+  ([bindings then]
+     `(if-all-let ~bindings ~then nil))
+  ([bindings then else]
+     (if (seq bindings)
+       `(let [test# ~(bindings 1)]
+          (if test#
+            (let [~(bindings 0) test#]
+              (if-let ~(subvec bindings 2) ~then ~else))
+            ~else))
+       then)))
+
+(defmacro when-all-let
+  "Similar to when-let but supports multiple binding terms which have to be true
+  for the binding evaluation to continue."
+  [bindings & body]
+  `(if-all-let ~bindings (do ~@body)))
+
+;; ...alternatively:
+#_(defmacro when-all-let
+  ""
+  [bindings expr]
+  (if (seq bindings)
+    `(if-let [~(first bindings) ~(second bindings)]
+       (and-let ~(drop 2 bindings) ~expr))
+    expr))
