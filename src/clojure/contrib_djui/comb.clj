fdadd9e5758c7733e583d07a086c5dd8d7ad18db
Uwe Dauernheim <uwe@dauernheim.net>
2013-06-16 16:47:38 +0200

    Deprecated |> in favor of ->

--- src/clojure/contrib_djui/comb.clj
+++ src/clojure/contrib_djui/comb.clj
@@ -4,7 +4,11 @@
 
 
 (defn |>
-  "Piping/Thrushing, similar to F#'s or OCaml |>, or Haskells $."
-  {:added "1.0"}
+  "Piping/Thrushing, similar to F#'s or OCaml |>, or Haskells $.
+  Limitations: Args are not evaluated lazily and functions can only take one
+  argument (as last argument).
+  Deprecated with Clojure 1.5, use clojure.core/-> or clojure.core/->> instead."
+  {:added "1.0"
+   :deprecated "1.1"}
   [& args]
   (reduce #(%2 %) args))
